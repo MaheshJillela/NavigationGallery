ca9cd318ab62a6fb902457f3c9a59c1b711928c4
Mahesh jillela <128527065+MaheshJillela@users.noreply.github.com>
2024-01-23 15:25:19 +0530

    Add files via upload
    Developed a web page of Navigation Gallery, showcasing diverse navigation bar styles with downloadable source codes for users/web designers. Simplifying the process of choosing and implementing stylish navigation interfaces.

---
+++ index.js
@@ -0,0 +1,299 @@
+window.onload=function(){
+    let sd_tog=document.getElementById('sd_tog');
+    let side_nav=document.getElementById('side_nav');
+    let content=document.getElementById('content');
+    let sd_icon1=document.getElementById('sd_icon1');
+    let sd_icon2=document.getElementById('sd_icon2');
+
+    let menu1=document.getElementById('menu1');
+    let menu2=document.getElementById('menu2');
+    let menu3=document.getElementById('menu3');
+    let menu4=document.getElementById('menu4');
+    let menu5=document.getElementById('menu5');
+    let menu6=document.getElementById('menu6');
+    let menu7=document.getElementById('menu7');
+    let menu8=document.getElementById('menu8');
+    let menu9=document.getElementById('menu9');
+    let menu10=document.getElementById('menu10');
+
+    let menu_1=document.getElementById('menu_1');
+    let menu_2=document.getElementById('menu_2');
+    let menu_3=document.getElementById('menu_3');
+    let menu_4=document.getElementById('menu_4');
+    let menu_5=document.getElementById('menu_5');
+    let menu_6=document.getElementById('menu_6');
+    let menu_7=document.getElementById('menu_7');
+    let menu_8=document.getElementById('menu_8');
+    let menu_9=document.getElementById('menu_9');
+    let menu_10=document.getElementById('menu_10');
+
+    menu_1.style.borderBottom='4px solid lightseagreen';
+    menu_2.onclick=function(){
+        menu2.style.display='block';
+        menu_2.style.borderBottom='4px solid lightseagreen';
+        menu1.style.display='none';
+        menu3.style.display='none';
+        menu4.style.display='none';
+        menu5.style.display='none';
+        menu6.style.display='none';
+        menu7.style.display='none';
+        menu8.style.display='none';
+        menu9.style.display='none';
+        menu10.style.display='none';
+        
+        menu_1.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_4.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+    menu_1.onclick=function(){
+        menu_1.style.borderBottom='4px solid lightseagreen';
+        menu1.style.display='block';
+        menu2.style.display='none';
+        menu3.style.display='none';
+        menu4.style.display='none';
+        menu5.style.display='none';
+        menu6.style.display='none';
+        menu7.style.display='none';
+        menu8.style.display='none';
+        menu9.style.display='none';
+        menu10.style.display='none';
+        menu_2.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_4.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+    menu_3.onclick=function(){
+        menu_3.style.borderBottom='4px solid lightseagreen';
+        menu3.style.display='block';
+        
+        menu1.style.display='none';
+        menu2.style.display='none';
+        menu4.style.display='none';
+        menu5.style.display='none';
+        menu6.style.display='none';
+        menu7.style.display='none';
+        menu8.style.display='none';
+        menu9.style.display='none';
+        menu10.style.display='none';
+        menu_1.style.borderBottom='none';
+        menu_2.style.borderBottom='none';
+        menu_4.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+
+    menu_4.onclick=function(){
+        menu_4.style.borderBottom='4px solid lightseagreen';
+        menu4.style.display='block';
+        
+        menu1.style.display='none';
+        menu2.style.display='none';
+        menu3.style.display='none';
+        menu5.style.display='none';
+        menu6.style.display='none';
+        menu7.style.display='none';
+        menu8.style.display='none';
+        menu9.style.display='none';
+        menu10.style.display='none';
+        menu_1.style.borderBottom='none';
+        menu_2.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+
+    menu_5.onclick=function(){
+        menu_5.style.borderBottom='4px solid lightseagreen';
+        menu5.style.display='block';
+        
+        menu1.style.display='none';
+        menu2.style.display='none';
+        menu4.style.display='none';
+        menu3.style.display='none';
+        menu6.style.display='none';
+        menu7.style.display='none';
+        menu8.style.display='none';
+        menu9.style.display='none';
+        menu10.style.display='none';
+        menu_1.style.borderBottom='none';
+        menu_2.style.borderBottom='none';
+        menu_4.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+
+    menu_6.onclick=function(){
+        menu_6.style.borderBottom='4px solid lightseagreen';
+        menu6.style.display='block';
+        
+        menu1.style.display='none';
+        menu2.style.display='none';
+        menu4.style.display='none';
+        menu5.style.display='none';
+        menu3.style.display='none';
+        menu7.style.display='none';
+        menu8.style.display='none';
+        menu9.style.display='none';
+        menu10.style.display='none';
+        menu_1.style.borderBottom='none';
+        menu_2.style.borderBottom='none';
+        menu_4.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+
+    menu_7.onclick=function(){
+        menu_7.style.borderBottom='4px solid lightseagreen';
+        menu7.style.display='block';
+        
+        menu1.style.display='none';
+        menu2.style.display='none';
+        menu4.style.display='none';
+        menu5.style.display='none';
+        menu6.style.display='none';
+        menu3.style.display='none';
+        menu8.style.display='none';
+        menu9.style.display='none';
+        menu10.style.display='none';
+        menu_1.style.borderBottom='none';
+        menu_2.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+
+    menu_8.onclick=function(){
+        menu_8.style.borderBottom='4px solid lightseagreen';
+        menu8.style.display='block';
+        
+        menu1.style.display='none';
+        menu2.style.display='none';
+        menu4.style.display='none';
+        menu5.style.display='none';
+        menu6.style.display='none';
+        menu7.style.display='none';
+        menu3.style.display='none';
+        menu9.style.display='none';
+        menu10.style.display='none';
+        menu_1.style.borderBottom='none';
+        menu_2.style.borderBottom='none';
+        menu_4.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+
+    menu_9.onclick=function(){
+        menu_9.style.borderBottom='4px solid lightseagreen';
+        menu9.style.display='block';
+        
+        menu1.style.display='none';
+        menu2.style.display='none';
+        menu4.style.display='none';
+        menu5.style.display='none';
+        menu6.style.display='none';
+        menu7.style.display='none';
+        menu8.style.display='none';
+        menu3.style.display='none';
+        menu10.style.display='none';
+        menu_1.style.borderBottom='none';
+        menu_2.style.borderBottom='none';
+        menu_4.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+        menu_10.style.borderBottom='none';
+    }
+
+    menu_10.onclick=function(){
+        menu_10.style.borderBottom='4px solid lightseagreen';
+        menu10.style.display='block';
+        
+        menu1.style.display='none';
+        menu2.style.display='none';
+        menu4.style.display='none';
+        menu5.style.display='none';
+        menu6.style.display='none';
+        menu7.style.display='none';
+        menu8.style.display='none';
+        menu9.style.display='none';
+        menu3.style.display='none';
+        menu_1.style.borderBottom='none';
+        menu_2.style.borderBottom='none';
+        menu_4.style.borderBottom='none';
+        menu_5.style.borderBottom='none';
+        menu_6.style.borderBottom='none';
+        menu_7.style.borderBottom='none';
+        menu_8.style.borderBottom='none';
+        menu_9.style.borderBottom='none';
+        menu_3.style.borderBottom='none';
+    }
+
+    sd_icon2.style.display='none';
+    sd_icon1.onclick=function(){
+        side_nav.style.display='none';
+        content.style.width='100%';
+        content.style.left="0px";
+        sd_icon2.style.display='block';
+        content.style.height='fit-content';
+    }
+    sd_icon2.onclick=function(){
+        side_nav.style.display='block';
+        content.style.left='10px';
+        sd_icon2.style.display='none';
+        content.style.width='calc(100% - 135px)';
+        content.style.height='100%';
+    }
+
+
+    let menu_icon=document.getElementById('menu_icon');
+    menu_icon.onclick=function() {
+    var x = document.getElementById("menu_toggle");
+    if(window.innerWidth < 800){
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+      
+    } else {
+      x.style.display = "block";
+    }
+}
+  }
+  
+  
+}
